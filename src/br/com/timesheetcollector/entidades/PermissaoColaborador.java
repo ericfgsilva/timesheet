424cd2295e1c93c3a25153f689d42cb4ab7eedc9
Eric Silva <eric.fernando.gs@gmail.com>
2016-05-03 13:31:46 -0300

    Create repository class RepositorioColaborador RepositorioPerfilColaborador RepositorioPermissaoColaborador RepositorioTipoAtividade

---
+++ src/br/com/timesheetcollector/entidades/PermissaoColaborador.java
@@ -0,0 +1,100 @@
+package br.com.timesheetcollector.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Created by eric.silva on 02/05/2016.
+ */
+
+@Entity
+@Table(name = "tb_permissao_colaborador", schema = "public")
+public class PermissaoColaborador implements Serializable {
+
+    @Id
+    @Column(name = "CODIGO", unique = true)
+    private String codigo;
+
+    @Column(name = "DESCRICAO")
+    private String descricao;
+
+    @Column(name = "CRIAR")
+    private boolean criar;
+
+    @Column(name = "LER")
+    private boolean ler;
+
+    @Column(name = "ATUALIZAR")
+    private boolean atualizar;
+
+    @Column(name = "DELETAR")
+    private boolean deletar;
+
+    public PermissaoColaborador() {
+    }
+
+    public PermissaoColaborador(String codigo, String descricao, boolean criar, boolean ler, boolean atualizar, boolean deletar) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.criar = criar;
+        this.ler = ler;
+        this.atualizar = atualizar;
+        this.deletar = deletar;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isCriar() {
+        return criar;
+    }
+
+    public void setCriar(boolean criar) {
+        this.criar = criar;
+    }
+
+    public boolean isLer() {
+        return ler;
+    }
+
+    public void setLer(boolean ler) {
+        this.ler = ler;
+    }
+
+    public boolean isAtualizar() {
+        return atualizar;
+    }
+
+    public void setAtualizar(boolean atualizar) {
+        this.atualizar = atualizar;
+    }
+
+    public boolean isDeletar() {
+        return deletar;
+    }
+
+    public void setDeletar(boolean deletar) {
+        this.deletar = deletar;
+    }
+
+    @Override
+    public String toString() {
+        return "Permissão do colaborador: c�digo " + this.codigo + ", descri��o " + this.descricao + ", criar " + this.criar + ", ler " + this.ler + ", atualizar " + this.atualizar + ", deletar " + this.deletar + " ]";
+    }
+}
